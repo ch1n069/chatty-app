4ca0810d6456f2666fe1abf1667162b0f0a6aefb
Bruno Alpachino <brunolalpachino@gmail.com>
2024-06-11 11:41:36 +0300

    feat:=== socket io setup

--- server/index.js
+++ server/index.js
@@ -6,12 +6,26 @@ const app = express();
 const http = require("http");
 
 const cors = require("cors");
-
+const { Server } = require("socket");
 app.use(cors());
-
-// const port = requir
 const server = http.createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    // url to the react server
+    origin: "http://localhost:3000",
+    // specify the methods that are to be accepted
+    methods: ["GET", "POST"],
+  },
+});
+
+// start the emittion of events and the detection of the events
+io.on("connection", (socket) => {
+  // use the socket to grab what is in the connection
+  console.log("socket id", socket.id);
+});
+
+// disconnet user
 // set the listening port
 server.listen(3001, () => {
   console.log("server is running on port 3001");
